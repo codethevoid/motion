2a1a5fc49ee49bd6fc995e106887a3a09b18c5d9
rmthomas98 <rmthomas1998@gmail.com>
2024-12-19 12:20:32 -0500

    fix destory cookie

--- app/api/token/destroy/route.ts
+++ app/api/token/destroy/route.ts
@@ -3,6 +3,8 @@ import { withWallet } from "@/lib/auth/with-wallet";
 
 export const GET = withWallet(async () => {
   const response = NextResponse.json({ message: "Token destroyed" });
-  response.cookies.delete("wallet");
+  response.cookies.set("wallet", "", {
+    expires: new Date(0),
+  });
   return response;
 });
