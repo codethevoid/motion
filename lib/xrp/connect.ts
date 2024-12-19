12b8e9d8ffc69121876dfc1fa48c4754b61d9459
rmthomas98 <rmthomas1998@gmail.com>
2024-12-18 20:43:57 -0500

    update

--- lib/xrp/connect.ts
+++ lib/xrp/connect.ts
@@ -1,21 +1,9 @@
 import { Client } from "xrpl";
 
-const XRPL_URL = "wss://s1.ripple.com:51233";
+const XRPL_URL = "wss://xrplcluster.com";
 
 export const getXrpClient = async () => {
   const client = new Client(XRPL_URL);
-  try {
-    if (!client.isConnected()) {
-      await client.connect();
-    }
-    return client;
-  } catch (error) {
-    // Clean up if connection fails
-    try {
-      await client.disconnect();
-    } catch (disconnectError) {
-      console.error("Error disconnecting after failed connection:", disconnectError);
-    }
-    throw error;
-  }
+  await client.connect();
+  return client;
 };
