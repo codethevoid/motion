e34234f367f4b220c5198401e70b9e10a4ce57aa
rmthomas <rmthomas1998@gmail.com>
2024-12-28 10:28:05 -0500

    add wallet nav to new wallets

--- components/wallet/auth.tsx
+++ components/wallet/auth.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { useWallet } from "@/hooks/use-wallet";
 import { Receive } from "@/components/wallet/receive";
 import { InfoIcon } from "lucide-react";
+import { Nav } from "./nav";
 
 export const WalletAuth = ({ children }: { children: ReactNode }) => {
   const { wallet, error, isLoading } = useWallet();
@@ -18,6 +19,7 @@ export const WalletAuth = ({ children }: { children: ReactNode }) => {
   if (!isLoading && !wallet?.isFunded) {
     return (
       <div className="mx-auto max-w-md space-y-2">
+        <Nav navTab="receive" setNavTab={() => {}} />
         <div className="flex space-x-2 rounded-xl border border-blue-500/20 bg-blue-500/10 px-2 py-1.5">
           <InfoIcon className="relative top-0.5 size-4 text-blue-500" />
           <p className="text-[13px] text-blue-500">Send at least 1 XRP to activate your wallet.</p>
