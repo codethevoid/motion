325febc62366d2bbc87decd8d878fc7c609b1c44
rmthomas <rmthomas1998@gmail.com>
2025-01-12 20:52:43 -0500

    change server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import tokenRoutes from "./routes/token.routes";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors({ origin: ["http://localhost:3000", "https://motion.zip"], credentials: true }));
 app.use(express.json({ limit: "20mb" }));
