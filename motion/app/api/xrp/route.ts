20c560ce7256b16dcb52bec4d2816b137958a8f5
rmthomas <rmthomas1998@gmail.com>
2025-03-19 15:22:10 -0400

    update api route

--- motion/app/api/xrp/route.ts
+++ motion/app/api/xrp/route.ts
@@ -3,6 +3,10 @@ import { NextResponse } from "next/server";
 
 // we just return the price of XRP in USD
 export const GET = async () => {
+  if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true") {
+    return NextResponse.json({ error: "Maintenance mode is active" }, { status: 503 });
+  }
+
   const xrpPrice = await getXrpValueInUsd();
   return NextResponse.json(xrpPrice);
 };
